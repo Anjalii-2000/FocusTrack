8e7c7c16575a2aeef8ff56f01abb54a12bfef029
Anjali Bhardwaj <your-email@gmail.com>
2026-02-05 01:36:29 +0530

    favicon icon added, server configurations added

--- focus-track-server/server.js
+++ focus-track-server/server.js
@@ -1,18 +1,25 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-
+const PORT = process.env.PORT || 5000;
 const activityRoutes = require("./routes/activityRoutes");
 
 const app = express();
-app.use(cors());
+
 app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("FocusTrack™ Server is Running");
+});
 
 mongoose.connect("mongodb://127.0.0.1:27017/focustrack")
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.error(err));
 
-
+const io = new Server(server, {
+  cors: { origin: "*" },
+  methods: ["GET", "POST"]
+});
 app.use("/api", activityRoutes);
 
 app.listen(5000, () => {
